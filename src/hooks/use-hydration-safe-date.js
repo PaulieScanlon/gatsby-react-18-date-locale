a25d901ffb67769f5d45d7192b721a722b1de01f
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-10-20 09:01:54 +0100

    test: add suspense

--- src/hooks/use-hydration-safe-date.js
+++ src/hooks/use-hydration-safe-date.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 export const useHydrationSafeDate = (date) => {
-  const [safeDate, setSafeDate] = useState('--/--/--');
+  const [safeDate, setSafeDate] = useState();
 
   useEffect(() => {
     setSafeDate(new Date(date).toLocaleDateString());
