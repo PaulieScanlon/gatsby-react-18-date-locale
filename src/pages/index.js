478367faaf072150f687cf8abdc3d614acdae3c7
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-10-20 09:58:53 +0100

    test: remove new

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <main>
       <h1>Page</h1>
-      <time>{new Intl.DateTimeFormat(undefined, { dateStyle: 'short', timeStyle: 'short' }).format().split(',')[0]}</time>
+      <time>{Intl.DateTimeFormat(undefined, { dateStyle: 'short', timeStyle: 'short' }).format().split(',')[0]}</time>
     </main>
   );
 };
