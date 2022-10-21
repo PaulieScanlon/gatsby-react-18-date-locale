d84b62dccb031e75e5b79fc718384401fc6b6a7c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-10-21 08:45:17 +0100

    add format string

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  plugins: [
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'markdown-pages',
+        path: `${__dirname}/content/`
+      }
+    }
+  ]
+};
